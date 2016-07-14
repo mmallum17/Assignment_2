103ab812fff28a7c9eb9ea6437182de9485b7e11
Marcus Mallum <marcusmallum@gmail.com>
2016-07-13 21:07:42 -0500

    Update

--- wordsearch.c
+++ wordsearch.c
@@ -1,9 +1,30 @@
 /* Marcus Mallum */
 /* Assignment 2 */
+#include <stdio.h>
 
-void printAnswer(char *puzzle);
+void printAnswer(const char puzzle[][size], size_t size);
 
 int main(void)
 {
+    char puzzle[0][0];
+    size_t size;
+
+    /* Read in Word Search */
+
+    printAnswer(puzzle, size);
     return 0;
+}
+
+void printAnswer(const char puzzle[][size], size_t size)
+{
+    size_t row;
+    size_t column;
+    for(row = 0; row < size; ++row)
+    {
+        for (column = 0; column < size; ++column)
+        {
+            printf("%c ", puzzle[row][column]);
+        }
+        puts("");
+    }
 }
