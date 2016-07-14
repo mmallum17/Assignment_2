fcd429f2fdc86824f735f1455a8098d3c8c54fde
Marcus Mallum <marcusmallum@gmail.com>
2016-07-13 21:26:01 -0500

    Update

--- wordsearch.c
+++ wordsearch.c
@@ -1,21 +1,39 @@
 /* Marcus Mallum */
 /* Assignment 2 */
 #include <stdio.h>
+#include <ctype.h>
 
-void printAnswer(const char puzzle[][size], size_t size);
+void printAnswer(const char puzzle[][50], size_t size);
 
 int main(void)
 {
-    char puzzle[0][0];
-    size_t size;
+    char puzzle[50][50];
+    size_t size = 0;
+    size_t row = 0;
+    size_t column = 0;
+    char letter;
 
     /* Read in Word Search */
-
+    do
+    {
+        column = 0;
+        size = 0;
+        do
+        {
+            letter = getchar();
+            if(!(isspace(letter)))
+            {
+                puzzle[row][column++] = letter;
+                size++;
+            }
+        }while(letter != '\n');
+        row++;
+    }while(row < size);
     printAnswer(puzzle, size);
     return 0;
 }
 
-void printAnswer(const char puzzle[][size], size_t size)
+void printAnswer(const char puzzle[][50], size_t size)
 {
     size_t row;
     size_t column;
